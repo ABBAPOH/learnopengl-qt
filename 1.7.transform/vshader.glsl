981f8112f2931d253af74bcb7781a3501b14c077
Ivan Komissarov <ABBAPOH@gmail.com>
2018-07-21 15:22:57 +0300

    Simplify data in transform

--- 1.7.transform/vshader.glsl
+++ 1.7.transform/vshader.glsl
@@ -1,10 +1,8 @@
 #version 330 core
 
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 color;
-layout (location = 2) in vec2 texCoord;
+layout (location = 1) in vec2 texCoord;
 
-out vec3 ourColor;
 out vec2 TexCoord;
 
 uniform mat4 transform;
@@ -12,6 +10,5 @@ uniform mat4 transform;
 void main()
 {
     gl_Position = transform * vec4(position, 1.0);
-    ourColor = color;
     TexCoord = vec2(texCoord.x, 1.0f - texCoord.y);
 }
