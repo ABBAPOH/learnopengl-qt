49d84335b76a29d78b8e4e13233a7b20d8357c4b
Ivan Komissarov <ABBAPOH@gmail.com>
2018-07-21 14:11:25 +0300

    Add window example

---
+++ 1.3.window/window.cpp
@@ -0,0 +1,46 @@
+#include "window.h"
+
+#include <QDebug>
+#include <QCoreApplication>
+
+Window::Window()
+{
+    resize(640, 480);
+}
+
+void Window::initializeGL()
+{
+    if (!context()) {
+        qCritical() << "Can't get OGL context";
+        close();
+        return;
+    }
+
+    _funcs = context()->versionFunctions<QOpenGLFunctions_3_3_Core>();
+    if (!_funcs) {
+        qCritical() << "Can't get OGL 3.2";
+        close();
+        return;
+    }
+    _funcs->initializeOpenGLFunctions();
+
+    qInfo() << "real OGL version" << reinterpret_cast<const char *>(_funcs->glGetString(GL_VERSION));
+}
+
+void Window::resizeGL(int w, int h)
+{
+    if (!_funcs)
+        return;
+
+    _funcs->glViewport(0, 0, w, h);
+}
+
+void Window::paintGL()
+{
+    if (!_funcs)
+        return;
+
+    _funcs->glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    _funcs->glClear(GL_COLOR_BUFFER_BIT);
+}
+
