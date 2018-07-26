3d419ea12dddfb626b19a42b254e3fd61a11625b
Ivan Komissarov <ABBAPOH@gmail.com>
2018-07-27 00:32:06 +0300

    Remove ivo from triangle

--- 1.4.0.triangle/window.h
+++ 1.4.0.triangle/window.h
@@ -24,7 +24,6 @@ private:
 private:
     QOpenGLFunctions_3_3_Core *m_funcs {nullptr};
     QOpenGLBuffer m_vbo {QOpenGLBuffer::VertexBuffer};
-    QOpenGLBuffer m_ibo {QOpenGLBuffer::IndexBuffer};
     QOpenGLVertexArrayObject m_vao;
     std::unique_ptr<QOpenGLShaderProgram> m_program;
 };
