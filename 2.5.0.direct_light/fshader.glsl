1f75b7f063ccb096902e60ab2d1c716ac0ebf3dd
Ivan Komissarov <ABBAPOH@gmail.com>
2018-08-05 12:55:52 +0300

    Add 2.5.0.direct_light example

---
+++ 2.5.0.direct_light/fshader.glsl
@@ -0,0 +1,48 @@
+#version 330 core
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
+
+struct Light {
+//    vec3 position; // Unused for the direct light
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec2 TexCoords;
+in vec3 FragPos;
+in vec3 Normal;
+
+out vec4 color;
+
+uniform vec3 objectColor;
+uniform vec3 viewPos;
+
+uniform Light light;
+uniform Material material;
+
+void main()
+{
+    vec3 normal = normalize(Normal);
+    vec3 lightDirection = normalize(-light.direction);
+
+    float diff = max(dot(normal, lightDirection), 0.0);
+
+    // ambient & diffuse & specular color
+    vec3 viewDirection = normalize(viewPos - FragPos);
+    vec3 reflectDirection = reflect(-lightDirection, normal);
+    float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), material.shininess);
+
+    vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+    vec3 result = ambient + diffuse + specular;
+    color = vec4(result, 1.0f);
+}
