207c048e88bca8c818f93ccd9634db622747a426
Ivan Komissarov <ABBAPOH@gmail.com>
2018-10-22 22:09:10 +0200

    Add 2.6.lights example

---
+++ 2.6.lights/fshader.glsl
@@ -0,0 +1,88 @@
+#version 330 core
+
+#define NR_POINT_LIGHTS 4
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
+
+struct DirLight {
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+struct PointLight {
+    vec3 position;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec2 TexCoords;
+in vec3 FragPos;
+in vec3 Normal;
+
+out vec4 FragColor;
+
+uniform vec3 objectColor;
+uniform vec3 viewPos;
+
+uniform Material material;
+uniform DirLight dirLight;
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+
+void main()
+{
+    vec3 norm = normalize(Normal);
+    vec3 viewDir = normalize(viewPos - FragPos);
+
+    vec3 result = CalcDirLight(dirLight, norm, viewDir);
+    for (int i = 0; i < NR_POINT_LIGHTS; i++)
+        result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
+    //result += CalcSpotLight(spotLight, norm, FragPos, viewDir);
+
+    FragColor = vec4(result, 1.0);
+}
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    return (ambient + diffuse + specular);
+}
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    float distance    = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance +
+                             light.quadratic * (distance * distance));
+    vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    ambient  *= attenuation;
+    diffuse  *= attenuation;
+    specular *= attenuation;
+    return (ambient + diffuse + specular);
+}
---
+++ 2.6.lights/window.h
@@ -0,0 +1,56 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QtGui/QOpenGLBuffer>
+#include <QtGui/QOpenGLFunctions_3_3_Core>
+#include <QtGui/QOpenGLShaderProgram>
+#include <QtGui/QOpenGLTexture>
+#include <QtGui/QOpenGLVertexArrayObject>
+#include <QtGui/QOpenGLWindow>
+
+#include <memory>
+
+class Camera;
+
+class Window : public QOpenGLWindow
+{
+public:
+    Window();
+    ~Window() override;
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+    void keyPressEvent(QKeyEvent *event) override;
+
+private:
+    void toggleFullScreen();
+    void initializeCubeGeometry();
+    void initializeLampGeometry();
+    void initializeShaders();
+    void initializeTextures();
+    void paintCube();
+    void paintLamps();
+
+private:
+    QOpenGLFunctions_3_3_Core *m_funcs {nullptr};
+    std::unique_ptr<Camera> m_camera;
+    QOpenGLBuffer m_vbo {QOpenGLBuffer::VertexBuffer};
+    QOpenGLVertexArrayObject m_vao;
+    QOpenGLBuffer m_lampVbo {QOpenGLBuffer::VertexBuffer};
+    QOpenGLVertexArrayObject m_lampVao;
+//    QVector3D m_lightPos {1.2f, 1.0f, 2.0f};
+    QVector3D m_lightPositions[4] = {
+        { 0.7f,  0.2f,  2.0f},
+        { 2.3f, -3.3f, -4.0f},
+        {-4.0f,  2.0f, -12.0f},
+        { 0.0f,  0.0f, -3.0f}
+    };
+    std::unique_ptr<QOpenGLShaderProgram> m_program;
+    std::unique_ptr<QOpenGLShaderProgram> m_lampProgram;
+    std::unique_ptr<QOpenGLTexture> m_texture;
+    std::unique_ptr<QOpenGLTexture> m_textureSpecular;
+};
+
+#endif // WINDOW_H
